0d6eb1e02acbb9baa54c7397b76fdce35394c5d4
Manish <rdjmanishoo7@gmail.com>
2025-06-28 22:32:25 +0530

    Add Modules

--- user-auth-service/src/main/java/com/example/auth/service/MainApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.auth.service;
-import org.springframework.boot.SpringApplication;
-
-public class MainApplication {
-
-    public static void main(String[] args) {SpringApplication.run(MainApplication.class,args);}
-
-}
